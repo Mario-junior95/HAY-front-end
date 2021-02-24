4e1b4a82e267a6b17b9d1f2dafb2b5129d43d3a5
saeedeljurdi <saeed.el.jurdi@gmail.com>
2021-02-24 13:23:11 +0200

    Responsive design blog & contact us

--- hay-front/src/components/blog/Blog.js
+++ hay-front/src/components/blog/Blog.js
@@ -1,27 +1,26 @@
 import React, { useEffect, useState } from "react";
 import Navigation from "../navigation/Navigation";
 import "./Blog.css";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 import Footer from "../Footer/Footer";
-import Axios from 'axios';
+import Axios from "axios";
 import Swal from "sweetalert2";
 
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 AOS.init();
 const Blog = () => {
-
-  const [subscribersNum , setSubscribersNum] = useState([])
-  const [render , setRender] = useState(false);
+  const [subscribersNum, setSubscribersNum] = useState([]);
+  const [render, setRender] = useState(false);
 
   useEffect(() => {
-    Axios.get('http://localhost:8000/api/subscribers')
-    .then((response)=>{
+    Axios.get("http://localhost:8000/api/subscribers").then((response) => {
       setSubscribersNum(response.data);
-    })
+    });
   }, [render]);
 
-    
   useEffect(() => {
     const getBlogs = async () => {
       const response = await fetch("http://localhost:8000/api/blogs");
@@ -29,8 +28,8 @@ const Blog = () => {
       setData([...data]);
       console.log(data);
       AOS.init({
-        duration : 3000
-      })
+        duration: 3000,
+      });
     };
     getBlogs();
   }, [render]);
@@ -39,9 +38,7 @@ const Blog = () => {
 
   const handleButton = () => {
     Swal.fire("Subscribed Successfully!", "You clicked the button!", "success");
-};
-
-
+  };
 
   const [email, setEmail] = useState("");
   const [status, setStatus] = useState("");
@@ -53,27 +50,27 @@ const Blog = () => {
   const [desc, setDesc] = useState("");
   const [img, setImg] = useState("");
 
-  const handleSubscribe = async(e) => {
+  const handleSubscribe = async (e) => {
     e.preventDefault();
     const data = new FormData();
-    data.append('email' , email);
+    data.append("email", email);
 
-    try{
-      await Axios.post('http://localhost:8000/api/subscribers' , data)
-      .then((response)=>{
-        console.log(response.data);
-        setRender(prev => !prev);
-        handleButton();
-        setStatus('');
-});
-    }catch(error){
-      if(error.response){
+    try {
+      await Axios.post("http://localhost:8000/api/subscribers", data).then(
+        (response) => {
+          console.log(response.data);
+          setRender((prev) => !prev);
+          handleButton();
+          setStatus("");
+        }
+      );
+    } catch (error) {
+      if (error.response) {
         setStatus(error.response.data.errors.email);
       }
     }
-  }
+  };
 
-  
   // const handleSubscribe = async () => {
   //   let formData = new FormData();
   //   formData.append("email", email);
@@ -97,8 +94,63 @@ const Blog = () => {
   return (
     <div>
       <Navigation />
+
+      <div className="subscribe____main">
+        <h1 style={{ paddingTop: "10%", fontSize: "40px" }} className="title">
+          HAY Blogs
+        </h1>
+        <h1 style={{ fontSize: "40px" }} className="titletwo">
+          We Rise By Lifting Others
+        </h1>
+      </div>
+      <span
+        className="numberofSubs"
+        style={{ fontSize: "30px", fontWeight: "bold", color: "white" }}
+      >
+        Subscribers : {Length}
+      </span>
+
+      <div className="Blogs-banner"></div>
+      <div
+        style={{ display: "grid", gridTemplateColumns: "1fr", marginTop: "5%" }}
+      >
+        <div className="wavves">
+          <span style={{ "--i": 1 }}>S</span>
+          <span style={{ "--i": 2 }}>U</span>
+          <span style={{ "--i": 3 }}>B</span>
+          <span style={{ "--i": 4 }}>S</span>
+          <span style={{ "--i": 5 }}>C</span>
+          <span style={{ "--i": 6 }}>R</span>
+          <span style={{ "--i": 7 }}>I</span>
+          <span style={{ "--i": 8 }}>B</span>
+          <span style={{ "--i": 9 }}>E</span>&nbsp;&nbsp;
+          <span style={{ "--i": 8 }}>H</span>
+          <span style={{ "--i": 8 }}>E</span>
+          <span style={{ "--i": 8 }}>R</span>
+          <span style={{ "--i": 8 }}>E</span>
+        </div>
+        <p className="status___subs">{status}</p>
+        <input
+          className="input______subscribers"
+          type="text"
+          style={{ margin: "0 auto", borderRadius: "25px", fontWeight: "bold" }}
+          placeholder="Email address"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </div>
+
+      <button
+        style={{
+          borderRadius: "15px",
+        }}
+        className="button___subs"
+        onClick={handleSubscribe}
+      >
+        SUBSCRIBE
+      </button>
       <div className="language_____switch___blog">
-        <span style={{color:"white", fontWeight:"bold"}}>En</span>{" "}
+        <span style={{ color: "black", fontWeight: "bold" }}>En</span>{" "}
         <label className="switch">
           <input
             type="checkbox"
@@ -108,66 +160,27 @@ const Blog = () => {
           />
           <span className="slider round"></span>
         </label>
-        <span style={{color:"white" , fontWeight:"bold"}}>Ar</span>
+        <span style={{ color: "black", fontWeight: "bold" }}>Ar</span>
       </div>
-      <div className="subscribe____main">
-        <h1 style={{paddingTop:"10%" , fontSize:"40px"}} className="title">HAY Blogs</h1>
-        <h1 style={{fontSize:"40px"}} className="titletwo">We Rise By Lifting Others</h1>
-
-      {/* 
-      <p className="lang__switch" onClick={() => setArabic(!arabic)}>
-        Change to {arabic ? "English" : "Arabic"}
-      </p>*/}
-  
-  </div>
-  <span className="numberofSubs" style={{fontSize:"30px" , fontWeight:"bold" , color:"white"}}>Subscribers : {Length}</span>
-  
-  <div className="Blogs-banner">
-      </div>
-      <div style={{display:"grid" , gridTemplateColumns:"1fr" , marginTop:"5%"}}>
-      <div className="wavves">
-      <span style={{"--i":1}}>S</span>
-      <span style={{"--i":2}}>U</span>
-      <span style={{"--i":3}}>B</span>
-      <span style={{"--i":4}}>S</span>
-      <span style={{"--i":5}}>C</span>
-      <span style={{"--i":6}}>R</span>
-      <span style={{"--i":7}}>I</span>
-      <span style={{"--i":8}}>B</span>
-      <span style={{"--i":9}}>E</span>&nbsp;&nbsp;
-      <span style={{"--i":8}}>H</span>
-      <span style={{"--i":8}}>E</span>
-      <span style={{"--i":8}}>R</span>
-      <span style={{"--i":8}}>E</span>
-      </div>
-      <p className="status___subs">{status}</p>
-      <input
-          className="input______subscribers"
-          type="texts" style={{margin:"0 auto" , borderRadius:"25px" }}
-          placeholder="   Email address"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        /></div>
-
-        <button  id="signin" style={{borderRadius:"15px" , marginTop:"2%"}} className="button___subs" onClick={handleSubscribe}>
-          SUBSCRIBE
-        </button>
       <div className="blogs____wrapper">
-       
         <div data-aos="fade-up" className="blogs___one">
           {data.map((blog) => {
             let descEn = blog.description_en;
             descEn = descEn.split(" ");
-            descEn = descEn.slice(0, 2);
+            descEn = descEn.slice(0, 5);
             descEn = descEn.join(" ");
             let descAr = blog.description_ar;
             descAr = descAr.split(" ");
-            descAr = descAr.slice(0, 2);
+            descAr = descAr.slice(0, 5);
             descAr = descAr.join(" ");
             let created_at = JSON.stringify(blog.created_at);
             created_at = created_at.slice(1, 11);
             return arabic ? (
-              <div  className="blog____two" key={blog.id}>
+              <div
+                className="blog____two"
+                key={blog.id}
+                style={{ borderRadius: "25px" }}
+              >
                 <div className="blog____image">
                   <img
                     className="image_______blog"
@@ -177,11 +190,12 @@ const Blog = () => {
                 </div>
                 <div className="body_____blog">
                   <div>
-                    <p>{blog.title_ar}</p>
-                    <p>{descAr}</p>
-                    <p>{created_at}</p>
+                    <h1 className="title_____blog_ar">{blog.title_ar}</h1>
+                    <p className="desc______blog_ar">{descAr}</p>
+                    <p className="time______blog_ar">{created_at}</p>
                   </div>
-                  <button id="signin" style={{borderRadius:"15px"}}
+                  <button
+                    className="readmore__blog"
                     onClick={() => {
                       setShow(true);
                       setId(blog.id);
@@ -191,6 +205,11 @@ const Blog = () => {
                     }}
                   >
                     اقرأ أكثر
+                    <FontAwesomeIcon
+                      icon={faAngleRight}
+                      className="event-icon"
+                      style={{ marginLeft: "2em" }}
+                    />
                   </button>
                   <p></p>
                 </div>
@@ -199,6 +218,7 @@ const Blog = () => {
               <div className="blog____two" key={blog.id}>
                 <div className="blog____image">
                   <img
+                    style={{ width: "100" }}
                     className="image_______blog"
                     src={`http://localhost:8000/storage/${blog.image}`}
                     alt=""
@@ -206,11 +226,12 @@ const Blog = () => {
                 </div>
                 <div className="body_____blog">
                   <div>
-                    <p>{blog.title_en}</p>
-                    <p>{descEn}...</p>
-                    <p>{created_at}</p>
+                    <h1 className="title_____blog_en">{blog.title_en}</h1>
+                    <p className="desc______blog_en">{descEn}...</p>
+                    <p className="time______blog_en">{created_at}</p>
                   </div>
-                  <button id="signin" style={{borderRadius:"15px" , width:"110px"}}
+                  <button
+                    className="readmore__blog"
                     onClick={() => {
                       setShow(true);
                       setId(blog.id);
@@ -220,6 +241,11 @@ const Blog = () => {
                     }}
                   >
                     Read more
+                    <FontAwesomeIcon
+                      icon={faAngleRight}
+                      className="event-icon"
+                      style={{ marginLeft: "2em" }}
+                    />
                   </button>
                   <p></p>
                 </div>
@@ -233,19 +259,21 @@ const Blog = () => {
           <div className="model__________main">
             <div className="model_______wrap">
               <div className="model_______wrap__two">
-                <p onClick={() => setShow(false)} className="x">
-                  x
-                </p>
-                <div className="blog____image">
-                  <img
-                    src={`http://localhost:8000/storage/${img}`}
-                    alt=""
-                    className="image___blog"
-                  />
-                </div>
-                <div className="body_____blog">
-                  <p className="title">{title}</p>
-                  <p className="desc">{desc}</p>
+                <div className="blogs___one">
+                  <p onClick={() => setShow(false)} className="x">
+                    x
+                  </p>
+                  <div className="blog____image">
+                    <img
+                      src={`http://localhost:8000/storage/${img}`}
+                      alt=""
+                      className="image___blog"
+                    />
+                  </div>
+                  <div className="body_____blog">
+                    <h1>{title}</h1>
+                    <p>{desc}</p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -254,7 +282,7 @@ const Blog = () => {
       ) : (
         false
       )}
-      <Footer/>
+      <Footer />
     </div>
   );
 };
--- hay-front/src/components/contact/Contact.js
+++ hay-front/src/components/contact/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Navigation from "../navigation/Navigation";
 import "./Contact.css";
 import Logo from "../Imagesss/logo.svg";
@@ -10,9 +10,9 @@ import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
-import Footer from '../Footer/Footer';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import Footer from "../Footer/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 AOS.init();
 const Contact = () => {
@@ -26,12 +26,11 @@ const Contact = () => {
   const [emailStatus, setEmailStatus] = useState("");
   const [messageStatus, setMessageStatus] = useState("");
 
-
   useEffect(() => {
     AOS.init({
-      duration : 3000
-    })
-  }, [])
+      duration: 3000,
+    });
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -83,31 +82,55 @@ const Contact = () => {
   return (
     <div>
       <Navigation />
-     
+
       <div className="join__us__contact">
-      <div className="contact-banner">
-        <div className="join__us__contact__title" style={{paddingBottom:"18%"}}>
-          {/* <h1 className="title____contact_front">Contact us</h1> */}
-        <h1 style={{paddingTop:"10%" , fontSize:"40px"}} className="title">Contact Us</h1>
-         <h3>You can always feel free to contact us for inquiries,<br></br> requests and other concerns.</h3>
+        <div className="contact-banner">
+          <div
+            className="join__us__contact__title"
+            style={{ paddingBottom: "26%" }}
+          >
+            {/* <h1 className="title____contact_front">Contact us</h1> */}
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <h1
+              style={{ paddingTop: "10%", fontSize: "40px" }}
+              className="title"
+            >
+              Contact Us
+            </h1>
+            <h3>
+              You can always feel free to contact us for inquiries,<br></br>{" "}
+              requests and other concerns.
+            </h3>
+          </div>
         </div>
-        </div>
-            <img data-aos="fade-left" style={{marginTop:"5%"}} className="contact__us__img" src={Logo} alt="" width="400px" />
-            <div  className="contact__form__container">
+        <img
+          data-aos="fade-left"
+          style={{ marginTop: "5%" }}
+          className="contact__us__img"
+          src={Logo}
+          alt=""
+          width="400px"
+        />
+        <div className="contact__form__container">
           <div data-aos="fade-right" className="contact__form__card">
-          <div className="wavvez">
-      <span style={{"--i":1}}>C</span>
-      <span style={{"--i":2}}>o</span>
-      <span style={{"--i":3}}>n</span>
-      <span style={{"--i":4}}>t</span>
-      <span style={{"--i":5}}>a</span>
-      <span style={{"--i":6}}>c</span>
-      <span style={{"--i":7}}>t</span>&nbsp;&nbsp;
-      <span style={{"--i":8}}>U</span>
-      <span style={{"--i":9}}>s</span>
-      </div>
-            <p >{status}</p>
-            <p style={{color:"red"}}>{nameStatus}</p>
+            <div className="wavvez">
+              <span style={{ "--i": 1 }}>C</span>
+              <span style={{ "--i": 2 }}>o</span>
+              <span style={{ "--i": 3 }}>n</span>
+              <span style={{ "--i": 4 }}>t</span>
+              <span style={{ "--i": 5 }}>a</span>
+              <span style={{ "--i": 6 }}>c</span>
+              <span style={{ "--i": 7 }}>t</span>&nbsp;&nbsp;
+              <span style={{ "--i": 8 }}>U</span>
+              <span style={{ "--i": 9 }}>s</span>
+            </div>
+            <p>{status}</p>
+            <p style={{ color: "red" }}>{nameStatus}</p>
             <input
               className="global____inputs___contact"
               type="texts"
@@ -116,7 +139,7 @@ const Contact = () => {
               onChange={(e) => setName(e.target.value)}
               value={name}
             />
-            <p style={{color:"red"}}>{emailStatus}</p>
+            <p style={{ color: "red" }}>{emailStatus}</p>
             <input
               className="global____inputs___contact"
               type="emails"
@@ -125,26 +148,29 @@ const Contact = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
-            <p style={{color:"red"}}>{phone}</p>
+            <p style={{ color: "red" }}>{phone}</p>
             <input
               className="global____inputs___contact"
-              type="phone"
+              type="number"
               value={number}
               placeholder="Phone number"
               required
               onChange={(e) => setNumber(e.target.value)}
             />
-            <p style={{color:"red"}}>{messageStatus}</p>
+            <p style={{ color: "red" }}>{messageStatus}</p>
             <textarea
               className="global___textarea_________contact"
-              placeholder="Message" style={{borderRadius:"25px"}}
+              placeholder="Message"
+              style={{ borderRadius: "25px" }}
               value={message}
               cols="26"
               rows="5"
               onChange={(e) => setMessage(e.target.value)}
               required
             />
-            <button id="signin" style={{borderRadius:"15px" , width:"30%"}}
+            <button
+              // id="signin"
+              style={{ borderRadius: "15px", width: "30%" }}
               className="contact__form__btn"
               type="submit"
               onClick={(e) => handleSubmit(e)}
@@ -171,7 +197,7 @@ const Contact = () => {
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
